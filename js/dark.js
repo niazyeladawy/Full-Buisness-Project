2907fe36fdb5e0ad7ef41c58fb9ffb75bdaf8376
niazyeladawy <niazyadawy2@gmail.com>
2020-12-17 12:30:00 +0200

    my first commit

---
+++ js/dark.js
@@ -0,0 +1,8 @@
+
+  /*
+  function toggleDarkLight() {
+    var myheader = document.getElementById("header");
+    var currentClass = myheader.className;
+    myheader.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  }
+  */
